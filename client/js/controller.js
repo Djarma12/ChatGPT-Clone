d9645fb796ba82a1fd557ceb4cbf64200561886f
Dusan Madjar <112414082+Djarma12@users.noreply.github.com>
2023-06-01 22:54:25 +0200

    Add files via upload

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,15 +1,42 @@
 import * as model from "./model.js";
+import AuthWithListener from "./model.js";
 import * as helpers from "./helpers.js";
 import ChatView from "./views/chatView.js";
+import DbView from "./views/dbView.js";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import chatView from "./views/chatView.js";
 
-const form = ChatView.form;
+// model.getBotResponse("prompt");
+
+const formChat = ChatView.form;
 const listView = ChatView.listView;
-let isSubmitting = false;
+let isSubmitting = false; // With this variable we disable sending a prompt to the server until the response is completed
+let userFB;
+let btnLogInOut = document.querySelector(".sidebar__login");
+
+const auth = getAuth();
+
+//////////////////////////////////////////
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    userFB = new AuthWithListener(user);
+    await userFB.getAllPosts();
+    DbView.renderAllPosts(userFB.allPostsColId);
+    DbView.handleActivePosts(handleActivePosts);
+
+    DbView.logInOut(btnLogInOut, true);
+  } else {
+    DbView.logInOut(btnLogInOut, false);
+    DbView.clearSidebarForm();
+    ChatView.clearChat();
+    console.log("no");
+  }
+});
 
 const handleSubmit = async (e) => {
   e.preventDefault();
   // Get input text
-  const prompt = new FormData(form).get("prompt");
+  const prompt = new FormData(formChat).get("prompt");
 
   // If prompt don't exist, stop function
   if (prompt === "" || isSubmitting) {
@@ -21,6 +48,7 @@ const handleSubmit = async (e) => {
   ChatView.clearInput();
 
   try {
+    console.log(userFB);
     const botResponsePromise = model.getBotResponse(prompt);
     const timerPromise = helpers.timerPromise(
       botResponsePromise,
@@ -29,9 +57,21 @@ const handleSubmit = async (e) => {
     );
 
     const parsedData = await Promise.race([botResponsePromise, timerPromise]);
-    console.log(parsedData);
-    // Wait for the data to be printed so that the user cannot start the handleSubmit function again
+    // const parsedData = "Parsed data";
+    // console.log(parsedData);
+    if (userFB) {
+      console.log(parsedData);
+      console.log(prompt);
+      const obj = { prompt: prompt, parsedData: parsedData };
+      if (userFB.activeCollectionId === "newchat") {
+        await userFB.addCollection(obj);
+        DbView.renderSinglePosts(userFB.idNameCollection);
+      } else {
+        await userFB.addDocument(obj);
+      }
+    }
     await ChatView.renderBotResponse(parsedData);
+    // Wait for the data to be printed so that the user cannot start the handleSubmit function again
     isSubmitting = false;
   } catch (err) {
     isSubmitting = false;
@@ -39,15 +79,45 @@ const handleSubmit = async (e) => {
   }
 };
 
-form.addEventListener("submit", handleSubmit);
-form.querySelector("#input-icon").addEventListener("click", handleSubmit);
+formChat.addEventListener("submit", handleSubmit);
+formChat.querySelector("#input-icon").addEventListener("click", handleSubmit);
 
 window.addEventListener("load", helpers.scrollTop(listView));
 
-document
-  .querySelector(".login")
-  .addEventListener("click", model.signInWithGoogle);
+// Handle active posts here and displayed them all
+const handleActivePosts = async function (id) {
+  // console.log("Active posts ID:", id);
+  await userFB.getData(id);
+  ChatView.clearChat();
+  userFB.dataSinglePost.forEach((obj) => {
+    chatView.renderBotResponse(obj.prompt, "user");
+    chatView.renderBotResponse(obj.parsedData, "parsedData");
+  });
+};
 
-if (module.hot) {
-  module.hot.accept();
-}
+document
+  .querySelector(".sidebar__login")
+  .addEventListener("click", async (e) => {
+    console.log(e.target.textContent);
+    try {
+      if (e.target.textContent === "Log in") {
+        await model.signInWithGoogle();
+      } else {
+        await model.singOutWithGoogle();
+      }
+    } catch (err) {
+      ChatView.alertError(err);
+    }
+  });
+
+document.querySelector(".form__list").addEventListener("click", async (e) => {
+  if (e.target.closest(".form__delete") && userFB) {
+    e.preventDefault();
+    const dataIndex = e.target.closest(".form__item");
+    const id = dataIndex.querySelector(".form__radio").id;
+    const labelText = dataIndex.querySelector(".form__label").textContent;
+    await userFB.deleteCollection({ id: id, namecollection: labelText });
+    DbView.deleteActivePosts(dataIndex);
+    ChatView.clearChat();
+  }
+});
--- client/js/model.js
+++ client/js/model.js
@@ -1,7 +1,24 @@
 import { API_URL } from "./config.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
 import { app, db } from "./firebaseConfig.js";
-import { collection, addDoc, getDocs, setDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  addDoc,
+  getDoc,
+  setDoc,
+  getDocs,
+  updateDoc,
+  arrayUnion,
+  deleteDoc,
+} from "firebase/firestore";
+
+const usersRef = collection(db, "users");
 
 export const getBotResponse = async function (prompt) {
   try {
@@ -25,47 +42,161 @@ export const getBotResponse = async function (prompt) {
       throw err;
     }
   } catch (err) {
+    console.error("Request Error:", err);
     // Catching errors if we cannot connect to the server
     throw `The connection to the server failed. ${err}`;
   }
 };
 
-export const signInWithGoogle = function () {
-  const auth = getAuth();
-  const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      console.log(user.uid);
-      // IdP data available using getAdditionalUserInfo(result)
+export const signInWithGoogle = async function () {
+  try {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
 
-      const dd = async function () {
-        const docRef = await setDoc(doc(db, "users", `${user.uid}`), data);
-        console.log("Document written with ID: ", docRef.id);
-      };
-      dd();
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+
+    const userDocRef = doc(usersRef, user.uid);
+    const userDocSnapshot = await getDoc(userDocRef);
+
+    if (!userDocSnapshot.exists()) {
+      await setDoc(userDocRef, {
+        id: user.uid,
+      });
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const singOutWithGoogle = function () {
+  const auth = getAuth();
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      console.log("Sign-out successful.");
     })
     .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
+      // An error happened.
+      console.log(error);
+      throw error;
     });
 };
 
-const dd = async function () {
-  const docRef = await addDoc(collection(db, "users"), {
-    name: "Tokyo",
-    country: "Japan",
-  });
-  console.log("Document written with ID: ", docRef.id);
-};
-// dd();
+export default class AuthWithListener {
+  user;
+  allPostsCol;
+  allPostsColId = [];
+  singlePost;
+  dataSinglePost = [];
+
+  activeCollectionId = "newchat";
+  idNameCollection;
+  constructor(user) {
+    this.user = user;
+    this.allPostsCol = doc(db, "users", user.uid);
+  }
+
+  async updateIdCollections(newId) {
+    await updateDoc(this.allPostsCol, {
+      id: arrayUnion(newId),
+    });
+  }
+
+  async getAllPosts() {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      if (doc.id === this.user.uid) {
+        console.log(doc.id, " => ", doc.data());
+        this.allPostsColId.push(doc.data());
+      }
+    });
+    console.log(this.allPostsColId);
+  }
+
+  // Get data from one post
+  async getData(idCollection) {
+    this.activeCollectionId = idCollection;
+    this.dataSinglePost = [];
+    this.singlePost = collection(db, "users", this.user.uid, `${idCollection}`);
+    this.singlePost = await getDocs(this.singlePost);
+
+    this.singlePost.forEach((doc) => {
+      // this.dataSinglePost.push({ id: doc.id, content: doc.data() });
+      console.log(doc.data());
+      this.dataSinglePost.push(doc.data());
+      // console.log(doc.id, " => ", doc.data());
+    });
+    this.dataSinglePost?.reverse();
+    console.log(this.dataSinglePost);
+  }
+
+  async addCollection(obj) {
+    console.log(this.user);
+    this.activeCollectionId = Date.now().toString();
+    console.log(`${this.activeCollectionId}`);
+    const userRef = doc(db, "users", this.user.uid); // replace 'user-id' with the actual user ID
+    const subCollectionRef = collection(userRef, `${this.activeCollectionId}`);
+    await addDoc(subCollectionRef, obj);
+    this.idNameCollection = {
+      id: this.activeCollectionId,
+      namecollection: obj.prompt,
+    };
+    console.log(this.idNameCollection);
+    this.updateIdCollections(this.idNameCollection);
+  }
+
+  async addDocument(obj) {
+    const reviewRef = doc(
+      collection(db, "users", this.user.uid, this.activeCollectionId)
+    );
+    await setDoc(reviewRef, obj);
+  }
+
+  // async updateField(obj) {
+  //   const washingtonRef = doc(db, "users", this.user.uid, "posts", "post");
+  //   // Set the "capital" field of the city 'DC'
+  //   await updateDoc(washingtonRef, obj);
+  // }
+
+  async deleteCollection(obj) {
+    // Delete subcollection
+    const postsRef = collection(db, "users", this.user.uid, `${obj.id}`);
+    const querySnapshot = await getDocs(postsRef);
+    querySnapshot.forEach((doc) => {
+      deleteDoc(doc.ref);
+    });
+    console.log("Subcollection deleted successfully.");
+
+    // Remove the deleted subcollection from the id array in the user document
+    const userRef = doc(db, "users", this.user.uid);
+
+    // Get the current user document data
+    const userSnapshot = await getDoc(userRef);
+    const userData = userSnapshot.data();
+    console.log(userData.id);
+    // Find the index of the object to be deleted in the id array
+    const index = userData.id.findIndex((item) => item.id === obj.id);
+    console.log(index);
+    if (index >= 0) {
+      // Remove the object from the id array
+      userData.id.splice(index, 1);
+
+      // Update the user document with the modified id array
+      await updateDoc(userRef, {
+        id: userData.id,
+      });
+
+      this.activeCollectionId = "newchat";
+      console.log("User field updated successfully.");
+    } else {
+      console.log("Object not found in the id array.");
+    }
+  }
+}
